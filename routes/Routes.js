51da3957c6bef35112826fbc347f7b48404211c0
mashayeakh <islammasayekh@gmail.com>
2025-05-16 12:05:12 +0600

    codded in mvc patteren

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createUser, findUser, updateUser, deleteUser } = require("./UserHandlers");
+const { createUser, findUser, updateUser, deleteUser } = require("../controller/UserController");
 
 const router = express.Router();
 
